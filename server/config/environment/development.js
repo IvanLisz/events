102839f024b4bddc7baed14e6b447abc99e5f687
Iván Nicolás Liszewski <ivan.nicolas.liszewski@gmail.com>
2015-05-21 02:27:06 -0300

    first commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/events-dev'
+  },
+
+  seedDB: true
+};
